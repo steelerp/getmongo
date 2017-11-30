8802957813ce3e42ed889af1249e245bb32143da
steelerp <32609461+steelerp@users.noreply.github.com>
2017-11-30 20:26:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.10.tgz
-&& tar -xvzf mongodb-linux-x86_64-3.4.10.tgz
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.10.tgz && tar -xvzf mongodb-linux-x86_64-3.4.10.tgz
